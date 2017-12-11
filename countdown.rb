48d0f1971037f9d704915d14c20a87141ed64381
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-11 17:21:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(integer)
   end
     puts "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep
+  x = integer(1.minute)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+end
